fe8dfa4b0c4ab4515df2ce451768b9cbf425091f
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-14 15:33:47 -0300

    fix: import file path

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../ui/dashboard/navbar/navbar"
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
-import styles from '../ui/dashboard.module.css'
+import styles from '../ui/dashboard/dashboard.module.css'
 
 export default function RootLayout({
   children,
