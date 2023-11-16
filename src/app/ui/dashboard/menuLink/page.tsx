315aa110018769340f1c5154ddf88cf31d255215
Valdir Silva <valdir.ti@gmail.com>
2023-11-15 23:13:28 -0300

    refact: change type to any for while

--- src/app/ui/dashboard/menuLink/page.tsx
+++ src/app/ui/dashboard/menuLink/page.tsx
@@ -4,27 +4,17 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
-type MenuLinkProps = {
-    item: {
-        icon: ReactNode,
-        title: string,
-        path: string
-    }
-}
-
-export default function MenuLink({ item }: MenuLinkProps) {
-
-    const {icon, path, title} = item
+export default function MenuLink({ item }: any) {
 
     const pathName = usePathname()
 
     return (
         <Link 
-            href={path} 
-            className={`p-3 flex items-center gap-3 m-1 rounded-md hover:bg-[--bgHover] ${pathName === path && 'bg-[--bgHover]'}`}
+            href={item.path} 
+            className={`p-3 flex items-center gap-3 m-1 rounded-md hover:bg-[--bgHover] ${pathName === item.path && 'bg-[--bgHover]'}`}
         >
-            {icon}
-            {title}
+            {item.icon}
+            {item.title}
         </Link>
     )
 }
