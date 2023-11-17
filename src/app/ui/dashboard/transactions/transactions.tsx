171389a536dda6f24cab9089eaaec96e0eacf5b1
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-17 09:08:36 -0300

    feat: create mocked transactions table

--- src/app/ui/dashboard/transactions/transactions.tsx
+++ src/app/ui/dashboard/transactions/transactions.tsx
@@ -1,20 +1,114 @@
+import Image from "next/image";
+
 export default function Transactions() {
   return (
     <div className="flex flex-1 flex-col p-4 bg-[--bgSoft] hover:bg-[--bgHover] rounded-md mt-4">
-      <h2>Latest Transactions</h2>
-      <table>
+      <h2 className="text-lg text-[--textSoft]">Latest Transactions</h2>
+      <table className="mt-4 ml-2">
         <thead>
-          <th>Name</th>
-          <th>Status</th>
-          <th>Date</th>
-          <th>Amount</th>
+          <tr>
+            <th className="text-start">Name</th>
+            <th className="text-start">Status</th>
+            <th className="text-start">Date</th>
+            <th className="text-start">Amount</th>
+          </tr>
         </thead>
         <tbody>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <tr>
+            <td className="">
+                <div className="flex items-center mt-4">
+                  <Image 
+                    alt="John Doe" 
+                    src='/noavatar.png' 
+                    width={28} 
+                    height={28}
+                    className="rounded-full"
+                    />
+                  <h4 className="ml-2">John Doe</h4>
+                </div>
+            </td>
+            <td>
+              <span className="bg-yellow-500 rounded-sm p-1">Pending</span>
+            </td>
+            <td>14.02.2023</td>
+            <td>R$3.200</td>
+          </tr>
+          <tr>
+            <td className="">
+                <div className="flex items-center mt-4">
+                  <Image 
+                    alt="John Doe" 
+                    src='/noavatar.png' 
+                    width={28} 
+                    height={28}
+                    className="rounded-full"
+                    />
+                  <h4 className="ml-2">John Doe</h4>
+                </div>
+            </td>
+            <td>
+              <span className="bg-slate-600 rounded-sm p-1">Done</span>
+            </td>
+            <td>14.02.2023</td>
+            <td>R$3.200</td>
+          </tr>
+          <tr>
+            <td className="">
+                <div className="flex items-center mt-4">
+                  <Image 
+                    alt="John Doe" 
+                    src='/noavatar.png' 
+                    width={28} 
+                    height={28}
+                    className="rounded-full"
+                    />
+                  <h4 className="ml-2">John Doe</h4>
+                </div>
+            </td>
+            <td>
+              <span className="bg-red-500 rounded-sm p-1">Cancelled</span>              
+            </td>
+            <td>14.02.2023</td>
+            <td>R$3.200</td>
+          </tr>
+          <tr>
+            <td className="">
+                <div className="flex items-center mt-4">
+                  <Image 
+                    alt="John Doe" 
+                    src='/noavatar.png' 
+                    width={28} 
+                    height={28}
+                    className="rounded-full"
+                    />
+                  <h4 className="ml-2">John Doe</h4>
+                </div>
+            </td>
+            <td>
+              <span className="bg-yellow-500 rounded-sm p-1">Pending</span>
+            </td>
+            <td>14.02.2023</td>
+            <td>R$3.200</td>
+          </tr>
+          <tr>
+            <td className="">
+                <div className="flex items-center mt-4">
+                  <Image 
+                    alt="John Doe" 
+                    src='/noavatar.png' 
+                    width={28} 
+                    height={28}
+                    className="rounded-full"
+                    />
+                  <h4 className="ml-2">John Doe</h4>
+                </div>
+            </td>
+            <td>
+              <span className="bg-slate-600 rounded-sm p-1">Done</span>
+            </td>
+            <td>14.02.2023</td>
+            <td>R$3.200</td>
+          </tr>
         </tbody>
       </table>
     </div>
