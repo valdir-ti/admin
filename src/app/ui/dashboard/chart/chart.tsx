b882ae16c43008f817e946f752ff95ae7f16439e
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-17 10:46:19 -0300

    feat: add data and style on chart

--- src/app/ui/dashboard/chart/chart.tsx
+++ src/app/ui/dashboard/chart/chart.tsx
@@ -1,68 +1,78 @@
 'use client'
 
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis, ResponsiveContainer } from 'recharts'
 
 export default function Chart() {
 
   const data = [
     {
-      "name": "Page A",
-      "uv": 4000,
-      "pv": 2400,
-      "amt": 2400
+      name: "Sun",
+      visit: 4000,
+      click: 2400,
     },
     {
-      "name": "Page B",
-      "uv": 3000,
-      "pv": 1398,
-      "amt": 2210
+      name: "Mon",
+      visit: 3000,
+      click: 1398,
     },
     {
-      "name": "Page C",
-      "uv": 2000,
-      "pv": 9800,
-      "amt": 2290
+      name: "Tue",
+      visit: 2000,
+      click: 3800,
     },
     {
-      "name": "Page D",
-      "uv": 2780,
-      "pv": 3908,
-      "amt": 2000
+      name: "Wed",
+      visit: 2780,
+      click: 3908,
     },
     {
-      "name": "Page E",
-      "uv": 1890,
-      "pv": 4800,
-      "amt": 2181
+      name: "Thu",
+      visit: 1890,
+      click: 4800,
     },
     {
-      "name": "Page F",
-      "uv": 2390,
-      "pv": 3800,
-      "amt": 2500
+      name: "Fri",
+      visit: 2390,
+      click: 3800,
     },
     {
-      "name": "Page G",
-      "uv": 3490,
-      "pv": 4300,
-      "amt": 2100
-    }
-  ]
+      name: "Sat",
+      visit: 3490,
+      click: 4300,
+    },
+  ];
 
   return (
-    <div className="flex flex-1 flex-col p-4 bg-[--bgSoft] hover:bg-[--bgHover] rounded-md mt-4">
-      <div className='w-9'>
-        <LineChart width={1060} height={330} data={data}
-          margin={{ top: 10, right: 30, left: 20, bottom: 0 }}>
-          <CartesianGrid strokeDasharray="3 3" />
+    <div className="flex flex-1 flex-col p-4 bg-[--bgSoft] hover:bg-[--bgHover] rounded-md mt-4 h-[400px]">
+      <h2 className="text-lg text-[--textSoft] mb-4">Weekly Recap</h2>
+      <ResponsiveContainer height='90%' width='100%'>
+          <LineChart
+            data={data}
+            margin={{
+              top: 10,
+              right: 35,
+              left: 20,
+              bottom: 10
+            }}
+          >
           <XAxis dataKey="name" />
           <YAxis />
-          <Tooltip />
+          <Tooltip contentStyle={{background: '#151c2c', border: '0'}}/>
           <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          <Line
+            type="monotone"
+            dataKey="visit"
+            stroke="#8884d8"
+            strokeDasharray="5 5"
+          />
+          <Line
+            type="monotone"
+            dataKey="click"
+            stroke="#82ca9d"
+            strokeDasharray="3 4 5 2"
+          />
         </LineChart>
-      </div>
+      </ResponsiveContainer>
     </div>
   )
 }
