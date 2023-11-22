07fa6f08dfa3771a8624a0868d80b7076563a788
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-22 16:54:54 -0300

    refact: remove warning from image

--- src/app/dashboard/products/[id]/page.tsx
+++ src/app/dashboard/products/[id]/page.tsx
@@ -1,45 +1,50 @@
 import Image from "next/image";
 
 export default function EditProduct() {
-  return (
-    <div className="flex gap-8">
-        <div className="w-2/6 p-12 bg-[--bgSoft] rounded-md mt-6 h-max">
-            <div className="w-[100%] h-[300px] font-bold flex items-center justify-center flex-col">
-                <Image alt="" src="/noproduct.jpg" width={300} height={180}/>
-                <h4 className="mt-2 text-[--textSoft] w-[75%]">Iphone</h4>
+    return (
+        <div className="flex gap-8">
+            <div className="w-2/6 p-12 bg-[--bgSoft] rounded-md mt-6 h-max">
+                <div className="w-[100%] h-[300px] font-bold flex items-center justify-center flex-col">
+                    <Image
+                        alt="Product"
+                        src="/noproduct.jpg"
+                        width="0"
+                        height="0"
+                        sizes="100vw"
+                        className="w-full h-auto"
+                    />
+                    <h4 className="mt-2 text-[--textSoft] w-full">Iphone</h4>
+                </div>
+            </div>
+            <div className="w-4/6 p-12 bg-[--bgSoft] rounded-md mt-6">
+                <form className="flex flex-col">
+
+                    <label htmlFor="name" className="text-xs">Name</label>
+                    <input type="text" name="name" id='name' placeholder="Name" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+
+                    <label htmlFor="price" className="text-xs">Price</label>
+                    <input type="number" id='price' name="price" placeholder="price" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+
+                    <label htmlFor="stock" className="text-xs">Stock</label>
+                    <input type="number" id='stock' name="stock" placeholder="32" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+
+                    <label htmlFor="size" className="text-xs">Size</label>
+                    <input type="number" name="size" id="size" placeholder="12" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+
+                    <label htmlFor="description" className="text-xs">Description</label>
+                    <textarea name="description" id="description" defaultValue="Description" cols={30} rows={2} className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+
+                    <label htmlFor="category" className="text-xs">Category</label>
+                    <select name="category" id="category" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2">
+                        <option value='kitchen'>kitchen</option>
+                        <option value='phone'>phone</option>
+                        <option value='computer'>computer</option>
+                    </select>
+
+                    <button className="w-full p-2 bg-teal-600 text-[--text] mt-4 rounded-md border-0">Submit</button>
+
+                </form>
             </div>
         </div>
-        <div className="w-4/6 p-12 bg-[--bgSoft] rounded-md mt-6">
-            <form className="flex flex-col">
-                
-                <label htmlFor="name" className="text-xs">Name</label>
-                <input type="text" name="name" id='name' placeholder="Name" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2"/>
-
-                <label htmlFor="price" className="text-xs">Price</label>
-                <input type="number" id='price' name="price" placeholder="price" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2"/>
-
-                <label htmlFor="stock" className="text-xs">Stock</label>
-                <input type="number" id='stock' name="stock" placeholder="32" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2"/>
-                
-                <label htmlFor="size" className="text-xs">Size</label>
-                <input type="number" name="size" id="size" placeholder="12" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2"/>
-
-                <label htmlFor="description" className="text-xs">Description</label>
-                <textarea name="description" id="description" cols={30} rows={2} className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2">
-                    Description
-                </textarea>
-
-                <label htmlFor="category" className="text-xs">Category</label>
-                <select name="category" id="category" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2">
-                    <option value='kitchen'>kitchen</option>
-                    <option value='phone'>phone</option>
-                    <option value='computer'>computer</option>
-                </select>
-
-                <button className="w-full p-2 bg-teal-600 text-[--text] mt-4 rounded-md border-0">Submit</button>
-
-            </form>
-        </div>
-    </div>
-  )
+    )
 }
--- src/app/dashboard/users/[id]/page.tsx
+++ src/app/dashboard/users/[id]/page.tsx
@@ -1,48 +1,52 @@
 import Image from "next/image";
 
 export default function EditUser() {
-  return (
-    <div className="flex gap-8">
-        <div className="w-2/6 p-12 bg-[--bgSoft] rounded-md mt-6 h-max">
-            <div className="w-[100%] h-[300px] font-bold flex items-center justify-center flex-col">
-                <Image alt="" src="/noavatar.png" width={300} height={180}/>
-                <h4 className="mt-2 text-[--textSoft] w-[75%]">John Doe</h4>
+    return (
+        <div className="flex gap-8">
+            <div className="w-2/6 p-12 bg-[--bgSoft] rounded-md mt-6 h-max">
+                <div className="w-[100%] h-[300px] font-bold flex items-center justify-center flex-col">
+                    <Image
+                        alt="User"
+                        src="/noavatar.png"
+                        width="0"
+                        height="0"
+                        sizes="100vw"
+                        className="w-full h-auto"
+                    />
+                    <h4 className="mt-2 text-[--textSoft] w-full">John Doe</h4>
+                </div>
+            </div>
+            <div className="w-4/6 p-12 bg-[--bgSoft] rounded-md mt-6">
+                <form className="flex flex-col">
+                    <label htmlFor="username" className="text-xs">Username</label>
+                    <input type="text" name="username" id="username" placeholder="John Doe" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+
+                    <label htmlFor="email" className="text-xs">Email</label>
+                    <input type="email" id="email" name="email" placeholder="john@mail.com" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+
+                    <label htmlFor="password" className="text-xs">Password</label>
+                    <input type="password" name="password" id="password" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+
+                    <label htmlFor="phone" className="text-xs">Phone</label>
+                    <input type="text" name="phone" id="phone" placeholder="+1233232332" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+
+                    <label htmlFor="address" className="text-xs">Address</label>
+                    <textarea name="address" id="address" defaultValue="Address" cols={30} rows={2} className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+                    <label htmlFor="isAdmin" className="text-xs">is Admin?</label>
+                    <select name="isAdmin" id="isAdmin" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2">
+                        <option value='true'>Yes</option>
+                        <option value='false'>No</option>
+                    </select>
+
+                    <label htmlFor="isActive" className="text-xs">is Active</label>
+                    <select name="isActive" id="isActive" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2">
+                        <option value='true'>Yes</option>
+                        <option value='false'>No</option>
+                    </select>
+
+                    <button className="w-full p-2 bg-teal-600 text-[--text] mt-4 rounded-md border-0">Submit</button>
+                </form>
             </div>
         </div>
-        <div className="w-4/6 p-12 bg-[--bgSoft] rounded-md mt-6">
-            <form className="flex flex-col">
-                <label htmlFor="username" className="text-xs">Username</label>
-                <input type="text" name="username" id="username" placeholder="John Doe" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2"/>
-
-                <label htmlFor="email" className="text-xs">Email</label>
-                <input type="email" id="email" name="email" placeholder="john@mail.com" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2"/>
-
-                <label htmlFor="password" className="text-xs">Password</label>
-                <input type="password" name="password" id="password" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2"/>
-
-                <label htmlFor="phone" className="text-xs">Phone</label>
-                <input type="text" name="phone" id="phone" placeholder="+1233232332" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2"/>
-
-                <label htmlFor="address" className="text-xs">Address</label>
-                <textarea name="address" id="address" cols={30} rows={2} className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2">
-                    Address
-                </textarea>
-
-                <label htmlFor="isAdmin" className="text-xs">is Admin?</label>
-                <select name="isAdmin" id="isAdmin" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2">
-                    <option value='true'>Yes</option>
-                    <option value='false'>No</option>
-                </select>
-
-                <label htmlFor="isActive" className="text-xs">is Active</label>
-                <select name="isActive" id="isActive" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2">
-                    <option value='true'>Yes</option>
-                    <option value='false'>No</option>
-                </select>
-
-                <button className="w-full p-2 bg-teal-600 text-[--text] mt-4 rounded-md border-0">Submit</button>
-            </form>
-        </div>
-    </div>
-  )
+    )
 }
