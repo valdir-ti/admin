1b4108f2536fe9e6a2ce11ef71a58b5eb4a7724e
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-22 15:41:58 -0300

    feat: create select box component

--- src/app/dashboard/products/add/page.tsx
+++ src/app/dashboard/products/add/page.tsx
@@ -1,26 +1,37 @@
+'use client'
+
 import Input from "@/app/ui/dashboard/input/input";
+import SelectBox, { SelectBoxOptionsProps } from "@/app/ui/dashboard/selectbox/selectbox";
+import { ChangeEvent, useState } from "react";
+
+const categories = ['Kitchen', 'Computer', 'Phone']
 
 export default function AddProduct() {
+
+    const options: SelectBoxOptionsProps[] = [
+        { label: 'Choose a category', value: '' },
+        ...categories.map((category) => ({ label: category, value: category }))
+    ]
+
+    const [value, setValue] = useState('')
+
+    const onChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setValue(event.target.value)
+    }
+
     return (
         <div className="bg-[--bgSoft] p-4 rounded-md mt-4">
             <form
                 className="flex flex-col"
             >
                 <div className="flex justify-between">
-                    <Input placeholder="title" name="title" required label="Title"/>
-                    <div className="flex flex-col w-[48%]">
-                        <label htmlFor="category" className="cursor-pointer mb-1">Category</label>
-                        <select
-                            name="category"
-                            id="category"
-                            className="w-full p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600"
-                            >
-                            <option value="general" className="text-[--text]">Choose a category</option>
-                            <option value="kitchen" className="text-[--text]">Kitchen</option>
-                            <option value="phone" className="text-[--text]">Phone</option>
-                            <option value="computer" className="text-[--text]">Computer</option>
-                        </select>
-                    </div>
+                    <Input placeholder="title" name="title" required label="Title" />                    
+                    <SelectBox 
+                        options={options} 
+                        onChange={onChange} 
+                        value={value} 
+                        label="Category"
+                    />
                 </div>
                 <div className="flex justify-between">
                     <Input placeholder="price" name="price" type="number" min='1' label="Price" />
--- src/app/dashboard/users/add/page.tsx
+++ src/app/dashboard/users/add/page.tsx
@@ -1,40 +1,60 @@
+'use client'
+
 import Input from "@/app/ui/dashboard/input/input";
+import SelectBox, { SelectBoxOptionsProps } from "@/app/ui/dashboard/selectbox/selectbox";
+import { ChangeEvent, useState } from "react";
+
+const isAdminOptions = ['isAdmin', 'yes', 'no']
+const isActiveOptions = ['isActive', 'yes', 'no']
 
 export default function AddUser() {
+
+    const adminOptions: SelectBoxOptionsProps[] = [
+        ...isAdminOptions.map((option) => ({ label: option, value: option }))
+    ]
+
+    const activeOptions: SelectBoxOptionsProps[] = [
+        ...isActiveOptions.map((option) => ({ label: option, value: option }))
+    ]
+
+    const [isAdminValue, setIsAdminValue] = useState('')
+    const [isActiveValue, setIsActiveValue] = useState('')
+
+    const onChangeAdmin = (event: ChangeEvent<HTMLSelectElement>) => {
+        setIsAdminValue(event.target.value)
+    }
+
+    const onChangeActive = (event: ChangeEvent<HTMLSelectElement>) => {
+        setIsActiveValue(event.target.value)
+    }
+
     return (
         <div className="bg-[--bgSoft] p-4 rounded-md mt-4">
             <form
                 className="flex flex-col"
             >
                 <div className="flex justify-between">
-                    <Input placeholder="name" name="name" required label="Name"/>
-                    <Input placeholder="email" name="email" type="email" label="Email"/>
+                    <Input placeholder="name" name="name" required label="Name" />
+                    <Input placeholder="email" name="email" type="email" label="Email" />
                 </div>
                 <div className="flex justify-between">
-                    <Input placeholder="password" name="password" type="password" required label="Password"/>
-                    <Input placeholder="phone" name="phone" required label="Phone"/>
+                    <Input placeholder="password" name="password" type="password" required label="Password" />
+                    <Input placeholder="phone" name="phone" required label="Phone" />
                 </div>
                 <div className="flex justify-between">
-                    <select
-                        name="isAdmin"
-                        id="isAdmin"
-                        className="w-[48%] p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600"
-                    >
-                        <option value='false' selected className="text-[--text]">is Admin?</option>
-                        <option value='true' className="text-[--text]">yes</option>
-                        <option value='false' className="text-[--text]">no</option>
-                    </select>
-                    <select
-                        name="isActive"
-                        id="isActive"
-                        className="w-[48%] p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600"
-                    >
-                        <option value='false' className="text-[--text]">is Active?</option>
-                        <option value='true' className="text-[--text]">yes</option>
-                        <option value='false' className="text-[--text]">no</option>
-                    </select>
+                    <SelectBox 
+                        options={adminOptions} 
+                        onChange={onChangeAdmin} 
+                        value={isAdminValue} 
+                        label="isAdmin" 
+                    />
+                    <SelectBox 
+                        options={activeOptions} 
+                        onChange={onChangeActive} 
+                        value={isActiveValue} 
+                        label="isActive" 
+                    />
                 </div>
-
                 <textarea
                     name="address"
                     id="address"
@@ -44,7 +64,7 @@ export default function AddUser() {
                 >
                     Address
                 </textarea>
-                
+
                 <button
                     className="w-full p-4 bg-teal-600 rounded-md color-[--text] border-0 cursor-pointer"
                 >
