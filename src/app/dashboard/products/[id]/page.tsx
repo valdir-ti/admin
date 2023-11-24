0478d25d3f8f6020f4bc32a8c9a6c89fcbdc54a8
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-24 11:20:27 -0300

    feat: get single user

--- src/app/dashboard/products/[id]/page.tsx
+++ src/app/dashboard/products/[id]/page.tsx
@@ -1,44 +1,55 @@
 import Image from "next/image";
 
-export default function EditProduct() {
+export default async function EditProduct({
+    params: { id },
+}: {
+    params: { id: string }
+}) {
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}products/${id}`, { cache: 'no-cache' })
+    const product = await response.json()
+
+    const { data: { title, description, price, stock, image, size, category, isActive } } = product
+    const productImage = image || "/noproduct.png"
+
     return (
         <div className="flex gap-8">
             <div className="w-2/6 p-12 bg-[--bgSoft] rounded-md mt-6 h-max">
                 <div className="w-[100%] h-[300px] font-bold flex items-center justify-center flex-col">
                     <Image
-                        alt="Product"
-                        src="/noproduct.jpg"
+                        alt={title}
+                        src={productImage}
                         width="0"
                         height="0"
                         sizes="100vw"
-                        className="w-[280px] h-auto"
+                        className="w-auto h-auto"
                     />
-                    <h4 className="mt-2 text-[--textSoft]">Iphone</h4>
+                    <h4 className="mt-2 text-[--textSoft]">{title}</h4>
                 </div>
             </div>
             <div className="w-4/6 p-12 bg-[--bgSoft] rounded-md mt-6">
                 <form className="flex flex-col">
 
                     <label htmlFor="name" className="text-xs">Name</label>
-                    <input type="text" name="name" id='name' placeholder="Name" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+                    <input type="text" name="name" id='name' placeholder="Name" value={title} className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
 
                     <label htmlFor="price" className="text-xs">Price</label>
-                    <input type="number" id='price' name="price" placeholder="price" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+                    <input type="number" id='price' name="price" placeholder="price" value={price} className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
 
                     <label htmlFor="stock" className="text-xs">Stock</label>
-                    <input type="number" id='stock' name="stock" placeholder="32" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+                    <input type="number" id='stock' name="stock" value={stock} placeholder="32" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
 
                     <label htmlFor="size" className="text-xs">Size</label>
-                    <input type="number" name="size" id="size" placeholder="12" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+                    <input type="number" name="size" id="size" placeholder="12" value={size} className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
 
                     <label htmlFor="description" className="text-xs">Description</label>
-                    <textarea name="description" id="description" defaultValue="Description" cols={30} rows={2} className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
+                    <textarea name="description" id="description" defaultValue="Description" value={description} cols={30} rows={2} className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2" />
 
                     <label htmlFor="category" className="text-xs">Category</label>
                     <select name="category" id="category" className="p-2 border-[2px] border-gray-500 rounded-md bg-[--bg] text-[--text] mb-2">
-                        <option value='kitchen'>kitchen</option>
-                        <option value='phone'>phone</option>
-                        <option value='computer'>computer</option>
+                        <option value='kitchen' selected={category === 'kitchen'}>kitchen</option>
+                        <option value='phone' selected={category === 'phone'}>phone</option>
+                        <option value='computer' selected={category === 'computer'}>computer</option>
                     </select>
 
                     <button className="w-full p-2 bg-teal-600 text-[--text] mt-4 rounded-md border-0">Submit</button>
