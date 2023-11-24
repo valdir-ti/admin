361d54d2732bfdff24222b46240244ced3424f92
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-24 15:24:14 -0300

    feat: configure toastify to restart server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
+'use client'
+
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
+
 export default function Home() {
+
+  useEffect(() => {
+
+    if (process.env.NEXT_PUBLIC_ENVIRONMENT !== "development") {
+
+      const restartServer = async () => {
+        await fetch(process.env.NEXT_PUBLIC_API_PROD!)
+          .then(() => { })
+          .catch((err) => {
+            toast.error("Error, please try again")
+          })
+      }
+
+      toast.promise(
+        restartServer,
+        {
+          pending: "Please wait while we are restarting our servers",
+          success: "Servers alive",
+          error: "Error, please try again",
+        }
+      )
+    }
+
+  }, [])
+
   return (
-    <main>Home page</main>
+    <main>
+      Home page
+    </main>
   )
 }
