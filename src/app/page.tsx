dbe28f631b361de65e6feb1e22054cd9275d237e
Valdir Silva <valdir.ti@gmail.com>
2023-12-02 23:46:45 -0300

    feat: add button to go dashboard and back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,34 @@
-'use client'
+"use client";
 
-import { useEffect } from 'react'
-import { toast } from 'react-toastify'
+import Link from "next/link";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function Home() {
-
   useEffect(() => {
-
     if (process.env.NEXT_PUBLIC_ENVIRONMENT !== "development") {
-
       const restartServer = async () => {
         await fetch(process.env.NEXT_PUBLIC_API_PROD!)
-          .then(() => { })
+          .then(() => {})
           .catch((err) => {
-            toast.error("Error, please try again")
-          })
-      }
+            toast.error("Error, please try again");
+          });
+      };
 
-      toast.promise(
-        restartServer,
-        {
-          pending: "Please wait while we are restarting our servers",
-          success: "Servers alive",
-          error: "Error, please try again",
-        }
-      )
+      toast.promise(restartServer, {
+        pending: "Please wait while we are restarting our servers",
+        success: "Servers alive",
+        error: "Error, please try again",
+      });
     }
-
-  }, [])
+  }, []);
 
   return (
     <main>
       Home page
+      <div className="mt-2 cursor-pointer">
+        <Link href="/dashboard">Go to Dashboard</Link>
+      </div>
     </main>
-  )
+  );
 }
--- src/app/ui/dashboard/sidebar/sidebar.tsx
+++ src/app/ui/dashboard/sidebar/sidebar.tsx
@@ -1,9 +1,20 @@
-import { MdAnalytics, MdAttachMoney, MdDashboard, MdHelpCenter, MdLogout, MdOutlineSettings, MdPeople, MdShoppingBag, MdSupervisedUserCircle, MdWork } from 'react-icons/md'
-import MenuLink from '../menuLink/menulink';
-import Image from 'next/image';
+import {
+  MdAnalytics,
+  MdAttachMoney,
+  MdDashboard,
+  MdHelpCenter,
+  MdLogout,
+  MdOutlineSettings,
+  MdPeople,
+  MdShoppingBag,
+  MdSupervisedUserCircle,
+  MdWork,
+} from "react-icons/md";
+import MenuLink from "../menuLink/menulink";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Sidebar() {
-
   const menuItems = [
     {
       title: "Pages",
@@ -69,33 +80,39 @@ export default function Sidebar() {
 
   return (
     <div className="sticky">
-      <div className='flex gap-4 mb-4 ml-2 items-center'>
-        <Image 
-          src='/noavatar.png' 
-          alt='avatar'
-          width={50} 
-          height={50} 
-          className='rounded-full object-cover cursor-pointer'
+      <div className="flex gap-4 mb-4 ml-2 items-center">
+        <Image
+          src="/noavatar.png"
+          alt="avatar"
+          width={50}
+          height={50}
+          className="rounded-full object-cover cursor-pointer"
         />
-        <div className='flex flex-col'>
-          <span className='font-bold'>Valdir Silva</span>
-          <span className='text-sm text-[--textSoft]'>Administrator</span>
+        <div className="flex flex-col">
+          <span className="font-bold">Valdir Silva</span>
+          <span className="text-sm text-[--textSoft]">Administrator</span>
         </div>
       </div>
-      <ul className=''>
-        {menuItems.map(cat => (
-          <li key={cat.title} className='text-[--textSoft] font-bold text-sm m-2'>
+      <ul className="">
+        {menuItems.map((cat) => (
+          <li
+            key={cat.title}
+            className="text-[--textSoft] font-bold text-sm m-2"
+          >
             <span>{cat.title}</span>
-            {cat.list.map(item => (
+            {cat.list.map((item) => (
               <MenuLink key={item.title} item={item} />
             ))}
           </li>
         ))}
       </ul>
-      <button className='flex items-center p-3 m-2 gap-2 cursor-pointer rounded-md text-[--textSoft] font-bold text-sm hover:bg-[--bgHover] bg-transparent border-0 w-[88%] ml-4'>
+      <Link
+        href="/"
+        className="flex items-center p-3 m-2 gap-2 cursor-pointer rounded-md text-[--textSoft] font-bold text-sm hover:bg-[--bgHover] bg-transparent border-0 w-[88%] ml-4"
+      >
         <MdLogout />
         Logout
-      </button>
+      </Link>
     </div>
-  )
+  );
 }
