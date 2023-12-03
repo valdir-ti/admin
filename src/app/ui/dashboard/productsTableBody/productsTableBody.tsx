6663ebe57ab4e90e79bd0723ae4e8a4b76baf5d7
Valdir Silva <valdir.ti@gmail.com>
2023-12-03 15:48:05 -0300

    feat: image product display

--- src/app/ui/dashboard/productsTableBody/productsTableBody.tsx
+++ src/app/ui/dashboard/productsTableBody/productsTableBody.tsx
@@ -5,61 +5,55 @@ import Link from "next/link";
 import { Fragment } from "react";
 
 type ProductsTableProps = {
-    data: Array<Product>
-}
+  data: Array<Product>;
+};
 
 export default function ProductsTableBody({ data }: ProductsTableProps) {
-  
-    return (
+  return (
+    <tbody>
+      {data.map((product) => {
+        const parsedDate = parseISO(product.createdAt);
+        const formattedDate = format(parsedDate, "dd.MM.yyyy");
 
-        <tbody>
-          {data.map((product) => {
+        const productImage = product.image || "/noproduct.jpg";
 
-            const parsedDate = parseISO(product.createdAt);
-            const formattedDate = format(parsedDate, 'dd.MM.yyyy')
-
-            const productImage = product.image || "/noproduct.jpg"
-
-            return (<Fragment key={product._id}>
-              <tr>
-                <td>
-                  <div className="flex items-center gap-2">
-                    <Image
-                      src={productImage}
-                      alt={product.title}
-                      width={40}
-                      height={40}
-                      className="rounded-full object-cover"
-                    />
-                    {product.title}
-                  </div>
-                </td>
-                <td>{product.description}</td>
-                <td>${product.price}</td>
-                <td>{formattedDate}</td>
-                <td>{product.stock}</td>
-                <td>
-                  <div className="gap-2 flex">
-                    <Link href={`/dashboard/products/${product._id}`}>
-                      <button
-                        className="p-1 text-[--text] border-0 cursor-pointer bg-teal-600 rounded-md min-w-[80px]"
-                      >
-                        View
-                      </button>
-                    </Link>
-                    <Link href="/">
-                      <button
-                        className="p-1 text-[--text] border-0 cursor-pointer bg-red-600 rounded-md min-w-[80px]"
-                      >
-                        Delete
-                      </button>
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            </Fragment>)
-          })}
-
-        </tbody>
-    )
+        return (
+          <Fragment key={product._id}>
+            <tr>
+              <td>
+                <div className="flex items-center gap-2 mb-2 mt-2">
+                  <Image
+                    src={productImage}
+                    alt={product.title}
+                    width={36}
+                    height={36}
+                    className="cursor-pointer rounded-sm"
+                  />
+                  {product.title}
+                </div>
+              </td>
+              <td>{product.description}</td>
+              <td>${product.price}</td>
+              <td>{formattedDate}</td>
+              <td>{product.stock}</td>
+              <td>
+                <div className="gap-2 flex">
+                  <Link href={`/dashboard/products/${product._id}`}>
+                    <button className="p-1 text-[--text] border-0 cursor-pointer bg-teal-600 rounded-md min-w-[80px]">
+                      View
+                    </button>
+                  </Link>
+                  <Link href="/">
+                    <button className="p-1 text-[--text] border-0 cursor-pointer bg-red-600 rounded-md min-w-[80px]">
+                      Delete
+                    </button>
+                  </Link>
+                </div>
+              </td>
+            </tr>
+          </Fragment>
+        );
+      })}
+    </tbody>
+  );
 }
