3e5f8b56a70d029ff6eb26eb6b2fffe83b99a900
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-12-12 09:57:52 -0300

    chore: autoclose toastify timeout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ToastContainer />
+        <ToastContainer autoClose={1000} />
         {children}
       </body>
     </html>
