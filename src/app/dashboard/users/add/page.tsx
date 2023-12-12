44d46f0f49b06422eff370a4fa099620f27be2f9
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-12-12 14:28:06 -0300

    feat: change field size on add user

--- src/app/dashboard/users/add/page.tsx
+++ src/app/dashboard/users/add/page.tsx
@@ -25,34 +25,34 @@ export default function Home() {
                 <div className="flex justify-between">
                     <div className="flex flex-col w-[49%]">
                         <label htmlFor="name">Name</label>
-                        <input placeholder="name" name="name" id="name" required autoComplete="off" className="w-full p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
+                        <input placeholder="name" name="name" id="name" required autoComplete="off" className="w-full p-4 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
                     </div>
                     <div className="flex flex-col w-[49%]">
                         <label htmlFor="email">Email</label>
-                        <input placeholder="email" name="email" id="email" type="email" autoComplete="off" className="w-full p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
+                        <input placeholder="email" name="email" id="email" type="email" autoComplete="off" className="w-full p-4 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
                     </div>
                 </div>
                 <div className="flex justify-between">
-                    <div className="flex flex-col w-[48%]">
+                    <div className="flex flex-col w-[49%]">
                         <label htmlFor="password">Password</label>
-                        <input placeholder="password" name="password" id="password" type="password" required className="w-full p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
+                        <input placeholder="password" name="password" id="password" type="password" required className="w-full p-4 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
                     </div>
-                    <div className="flex flex-col w-[48%]">
+                    <div className="flex flex-col w-[49%]">
                         <label htmlFor="phone">Phone</label>
-                        <input placeholder="phone" name="phone" id="phone" autoComplete="off" className="w-full p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
+                        <input placeholder="phone" name="phone" id="phone" autoComplete="off" className="w-full p-4 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
                     </div>
                 </div>
                 <div className="flex justify-between">
-                    <div className="flex flex-col w-[48%]">
+                    <div className="flex flex-col w-[49%]">
                         <label htmlFor="isAdmin">IsAdmin?</label>
-                        <select name="isAdmin" id="isAdmin" className={`p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600 mt-2 w-full`}>
+                        <select name="isAdmin" id="isAdmin" className={`p-4 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600 mt-2 w-full`}>
                             <option value="true">Yes</option>
                             <option value="false">No</option>
                         </select>
                     </div>
-                    <div className="flex flex-col w-[48%]">
+                    <div className="flex flex-col w-[49%]">
                         <label htmlFor="isActive">IsActive</label>
-                        <select name="isActive" id="isActive" className={`p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600 mt-2 w-full`}>
+                        <select name="isActive" id="isActive" className={`p-4 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600 mt-2 w-full`}>
                             <option value="true">Yes</option>
                             <option value="false">No</option>
                         </select>
@@ -60,7 +60,7 @@ export default function Home() {
                 </div>
                 <div className="flex flex-col w-full">
                     <label htmlFor="address">Address</label>
-                    <textarea name="address" id="address" defaultValue="Address" className="w-full p-6 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
+                    <textarea name="address" id="address" defaultValue="Address" className="w-full p-4 bg-[--bg] text-[--text] mb-6 rounded-md border-[1px] border-gray-600" />
                 </div>
                 <button
                     className="w-full p-4 bg-teal-600 rounded-md color-[--text] border-0 cursor-pointer"
