444e11d3252c5354f30834fa1f9cb214606086bd
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-12-13 16:16:43 -0300

    feat: update user model

--- src/app/types/user.ts
+++ src/app/types/user.ts
@@ -2,6 +2,9 @@ export type User = {
   _id: string
   name: string
   email: string
+  password: string
+  phone: string
+  address: string
   createdAt: string
   isAdmin: boolean
   isActive: boolean
