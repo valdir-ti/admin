313383862e8954209d9bfb54145107613b0c1e20
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-08 15:23:02 -0300

    feat: add confirm on delete todo

--- src/app/dashboard/todos/delete-form.tsx
+++ src/app/dashboard/todos/delete-form.tsx
@@ -1,6 +1,8 @@
+import { useState, FormEvent } from 'react'
 import { toast } from 'react-toastify'
 import { deleteTodoServerAction } from '@/app/actions/todos/delete-todo-action'
 import { DeleteButton } from '../../ui/dashboard/delete-button/delete-button'
+import { confirmAlert } from 'react-confirm-alert'
 
 type DeleteButtonProps = {
   id: string
@@ -8,19 +10,85 @@ type DeleteButtonProps = {
 }
 
 export default function DeleteForm({ id, disabled }: DeleteButtonProps) {
-  const deleteTodoClientAction = async (formData: FormData) => {
-    const result = await deleteTodoServerAction(formData)
-    if (result?.error) {
-      toast.error(result?.error)
-    } else {
-      toast.success('Todo deleted')
-    }
+  const [loading, setLoading] = useState(false)
+
+  const handleTodoDelete = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    confirmAlert({
+      customUI: ({ onClose }) => {
+        return (
+          <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+              <div className="fixed inset-0 transition-opacity">
+                <div className="absolute inset-0 bg-gray-500 opacity-5"></div>
+              </div>
+              <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <svg
+                      className="h-6 w-6 text-green-600"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">
+                      Deseja realmente excluir esse item?
+                    </h3>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                  <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                      onClick={async () => {
+                        setLoading(true)
+                        onClose()
+                        const result = await deleteTodoServerAction(id)
+                        if (result?.error) {
+                          toast.error(result?.error)
+                        } else {
+                          toast.success('Todo deleted')
+                        }
+                        setLoading(false)
+                      }}
+                    >
+                      Sim
+                    </button>
+                  </span>
+                  <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                      onClick={onClose}
+                    >
+                      Não
+                    </button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      }
+    })
   }
 
   return (
-    <form action={deleteTodoClientAction}>
+    <form onSubmit={handleTodoDelete}>
       <input type="hidden" name="id" value={id} />
-      <DeleteButton disabled={disabled} />
+      <DeleteButton disabled={disabled} loading={loading} />
     </form>
   )
 }
