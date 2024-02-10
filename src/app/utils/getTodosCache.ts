e0103672575cf104653b9b5a7c85886e769f5a3b
Valdir Silva <valdir.ti@gmail.com>
2024-02-09 23:33:29 -0300

    feat: add cache on user and products

--- src/app/utils/getTodosCache.ts
+++
@@ -1,7 +0,0 @@
-import { cache } from 'react'
-import { getTodos } from '@/app/services/api-todos'
-
-export const getTodosCache = cache(async () => {
-  const todos = await getTodos()
-  return todos
-})
