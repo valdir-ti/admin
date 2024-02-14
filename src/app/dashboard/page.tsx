827bda2c92119f8f8642c520dbfb8561aafddd07
Valdir Silva <valdir.ti@gmail.com>
2024-02-14 18:39:33 -0300

    feat: add total items on cards and redirect to correct pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,24 @@
-import Card from '../ui/dashboard/card/card'
-import Chart from '../ui/dashboard/chart/chart'
-import RightBar from '../ui/dashboard/rightbar/rightbar'
-import Transactions from '../ui/dashboard/transactions/transactions'
+import { getTodosServerAction } from '@/app/actions/todos/get-todos-action'
+import { getUsersServerAction } from '@/app/actions/users/get-users-action'
+import { getProductsServerAction } from '@/app/actions/products/get-products-action'
 
-export default function Home() {
+import Card from '@/app/ui/dashboard/card/card'
+import Chart from '@/app/ui/dashboard/chart/chart'
+import RightBar from '@/app/ui/dashboard/rightbar/rightbar'
+import Transactions from '@/app/ui/dashboard/transactions/transactions'
+
+export default async function Home() {
+  const users = await getUsersServerAction()
+  const products = await getProductsServerAction()
+  const todos = await getTodosServerAction()
+
   return (
     <div className="flex justify-between gap-4 mt-4">
       <div className="w-9/12 gap-2">
         <div className="flex justify-between">
-          <Card />
-          <Card />
-          <Card />
+          <Card item="Users" total={users.length} />
+          <Card item="Products" total={products.length} />
+          <Card item="Todos" total={todos.length} />
         </div>
         <div>
           <Transactions />
--- src/app/ui/dashboard/card/card.tsx
+++ src/app/ui/dashboard/card/card.tsx
@@ -1,14 +1,23 @@
+import Link from 'next/link'
 import { MdSupervisedUserCircle } from 'react-icons/md'
 
-export default function Card() {
+type CardProps = {
+  total: number
+  item: string
+}
+
+export default function Card({ total, item }: CardProps) {
   return (
-    <div className="p-4 bg-[--bgSoft] hover:bg-[--bgHover] rounded-md min-w-[32%] cursor-pointer">
+    <Link
+      href={`/dashboard/${item.toLowerCase()}`}
+      className="p-4 bg-[--bgSoft] hover:bg-[--bgHover] rounded-md min-w-[32%] cursor-pointer"
+    >
       <div className="flex items-center">
         <MdSupervisedUserCircle />
-        <h4 className="ml-3 font-medium">Total Users</h4>
+        <h4 className="ml-3 font-medium">Total {item}</h4>
       </div>
       <div className="mt-4 ml-7">
-        <h3 className="text-2xl font-bold">10.928</h3>
+        <h3 className="text-2xl font-bold">{total}</h3>
       </div>
       <div className="ml-7 mt-4 flex items-center text-sm">
         <h5 className="">
@@ -16,6 +25,6 @@ export default function Card() {
           previous week
         </h5>
       </div>
-    </div>
+    </Link>
   )
 }
