d8856d1892c6d09a594c33a791a55579a5346167
Valdir Silva <valdir.ti@gmail.com>
2024-02-14 18:14:45 -0300

    feat: add types colors variants as objects

--- src/app/ui/dashboard/dialog-confirm/dialog-confirm.tsx
+++ src/app/ui/dashboard/dialog-confirm/dialog-confirm.tsx
@@ -13,6 +13,21 @@ export default function DialogConfirm({
   message,
   type
 }: DialogConfirmProps) {
+  const typesColorVariantsCircleBackground = {
+    green: 'bg-green-100',
+    red: 'bg-red-100'
+  }
+
+  const typesColorVariantsSVG = {
+    green: 'text-green-600',
+    red: 'text-red-600'
+  }
+
+  const typesColorVariantsConfirmButton = {
+    green: 'bg-green-600 hover:bg-green-500',
+    red: 'bg-red-600 hover:bg-red-500'
+  }
+
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
       <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
@@ -23,10 +38,10 @@ export default function DialogConfirm({
         <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
           <div className="sm:flex sm:items-center">
             <div
-              className={`mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-${type}-100 sm:mx-0 sm:h-10 sm:w-10`}
+              className={` ${typesColorVariantsCircleBackground[type]} mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10`}
             >
               <svg
-                className={`h-6 w-6 text-${type}-600`}
+                className={`${typesColorVariantsSVG[type]} h-6 w-6`}
                 stroke="currentColor"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -49,7 +64,7 @@ export default function DialogConfirm({
             <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
               <button
                 type="button"
-                className={`inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-${type}-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-${type}-500 focus:outline-none focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5`}
+                className={`${typesColorVariantsConfirmButton[type]} inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 text-base leading-6 font-medium text-white shadow-sm focus:outline-none focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5`}
                 onClick={() => {
                   onClose()
                   onConfirm()
