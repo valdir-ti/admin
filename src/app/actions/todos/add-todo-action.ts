8a09c6c824c4f3cb998fb8f6a5d4012238f31c84
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-16 16:15:06 -0300

    feat: add min length field

--- src/app/actions/todos/add-todo-action.ts
+++ src/app/actions/todos/add-todo-action.ts
@@ -7,7 +7,7 @@ import { createTodo } from '@/app/services/api-todos'
 import { getErrorMessage } from '@/app/utils/getErrorMessage'
 
 const schema = z.object({
-  description: z.string()
+  description: z.string().min(2)
 })
 
 export const addTodoServerAction = async (formData: FormData) => {
