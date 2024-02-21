0b43fe0a8bbc297240e15f7722fb56a653d4fa1d
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-21 17:11:13 -0300

    feat: users and products table styles

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -18,8 +18,8 @@ export default async function Home() {
         </Link>
       </div>
       <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right">
-          <thead>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr className="text-lg text-slate-400">
               <th scope="col" className="px-6 py-3">
                 Title
