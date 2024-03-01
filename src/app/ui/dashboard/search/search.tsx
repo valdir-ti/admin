8499fc2c0216be5979f24adbaa1300ef648f8b0f
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-03-01 10:49:04 -0300

    feat: add close icon and handle clean input

--- src/app/ui/dashboard/search/search.tsx
+++ src/app/ui/dashboard/search/search.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { MdSearch } from 'react-icons/md'
+import { useState, useRef } from 'react'
+import { MdSearch, MdClose } from 'react-icons/md'
 import { useDebouncedCallback } from 'use-debounce'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
@@ -9,6 +10,8 @@ type SearchProps = {
 }
 
 export default function Search({ placeholder }: SearchProps) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [showCloseIcon, setShowCloseIcon] = useState(false)
   const searchParams = useSearchParams()
   const { replace } = useRouter()
   const pathname = usePathname()
@@ -19,8 +22,9 @@ export default function Search({ placeholder }: SearchProps) {
 
       params.set('page', '1')
 
-      if (e.target.value) {
-        e.target.value.length > 1 && params.set('q', e.target.value)
+      if (e.target.value && e.target.value.length > 1) {
+        params.set('q', e.target.value)
+        setShowCloseIcon(true)
       } else {
         params.delete('q')
       }
@@ -29,10 +33,26 @@ export default function Search({ placeholder }: SearchProps) {
     300
   )
 
+  const handleCleanSearch = () => {
+    const params = new URLSearchParams(searchParams)
+    if (inputRef.current) {
+      inputRef.current.value = ''
+    }
+    setShowCloseIcon(false)
+    params.set('page', '1')
+    params.delete('q')
+    replace(`${pathname}?${params}`)
+  }
+
   return (
     <div className="flex items-center gap-3 bg-[--bgHover] rounded-md p-2">
-      <MdSearch />
+      {!showCloseIcon ? (
+        <MdSearch />
+      ) : (
+        <MdClose className="cursor-pointer" onClick={handleCleanSearch} />
+      )}
       <input
+        ref={inputRef}
         type="text"
         placeholder={placeholder}
         className="bg-transparent border-0 text-[--bgHover] outline-none text-white"
