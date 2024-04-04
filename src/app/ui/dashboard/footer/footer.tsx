3151fd131db6f30184c48810814dbe2a754a83dd
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-04-04 16:01:11 -0300

    feat: footer margin top

--- src/app/ui/dashboard/footer/footer.tsx
+++ src/app/ui/dashboard/footer/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="flex items-center justify-between mt-5 text-[--textSoft] w-full">
+    <div className="flex items-center justify-between mt-2 sm:mt-5 text-[--textSoft] w-full">
       <div className="font-bold">Intratec Tecnologia</div>
       <div className="text-sm">® All rights reserved</div>
     </div>
