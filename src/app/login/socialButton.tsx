375ab2202f2bb2155fa50b0a88477343f89a04c5
Valdir Silva <valdir.ti@gmail.com>
2024-04-22 11:23:50 -0300

    feat: add github provider and create a social button

--- src/app/login/socialButton.tsx
+++ src/app/login/socialButton.tsx
@@ -1,18 +1,28 @@
-import { signIn } from 'next-auth/react'
 import Image from 'next/image'
+import { signIn } from 'next-auth/react'
 
-export default function GoogleButton() {
+type SocialButtonProps = {
+  alt: string
+  icon: string
+  signInService: string
+}
+
+export default function SocialButton({
+  alt,
+  icon,
+  signInService
+}: SocialButtonProps) {
   return (
     <button
       className="w-[18%] h-12 rounded-sm bg-teal-600 cursor-pointer border-0"
       onClick={() =>
-        signIn('google', {
+        signIn(signInService, {
           callbackUrl: '/dashboard'
         })
       }
     >
       <div className="w-full flex items-center justify-center">
-        <Image src="/g-icon.png" width={32} height={32} alt="Google Icon" />
+        <Image src={icon} width={32} height={32} alt={alt} />
       </div>
     </button>
   )
