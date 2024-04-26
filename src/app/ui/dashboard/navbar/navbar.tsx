28ecbe1b27a370aefe1d8d65dc0ae707c3ce4dff
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-04-26 14:33:19 -0300

    feat: show default search bar on dashboard page

--- src/app/ui/dashboard/navbar/navbar.tsx
+++ src/app/ui/dashboard/navbar/navbar.tsx
@@ -15,9 +15,8 @@ import { useSidebarMobileStore } from '@/store/sidebarMobileStore'
 import DropDownMenu from '@/app/ui/dashboard/dropdown-menu/dropdown-menu'
 
 export default function Navbar() {
-  const { handleOpen } = useSidebarMobileStore()
-
   const pathName = usePathname()
+  const { handleOpen } = useSidebarMobileStore()
   const [isHidden, setIsHidden] = useState(true)
   const dropdownRef = useRef<HTMLDivElement>(null)
 
@@ -58,15 +57,17 @@ export default function Navbar() {
           {pathName.split('/').pop()}
         </div>
       </div>
-      <div className="flex items-center gap-4">
-        <div className="hidden sm:flex items-center gap-3 bg-[--bgHover] rounded-md p-2">
-          <MdSearch />
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-transparent border-0 text-[--bgHover] outline-none"
-          />
-        </div>
+      <div className="flex items-center gap-4 h-10">
+        {pathName.split('/').pop() === 'dashboard' && (
+          <div className="hidden sm:flex items-center gap-3 bg-[--bgHover] rounded-md p-2">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="bg-transparent border-0 text-[--bgHover] outline-none"
+            />
+          </div>
+        )}
         <div className="flex gap-4 relative" ref={dropdownRef}>
           <MdOutlineChat size={20} className="cursor-pointer" />
           <MdNotifications size={20} className="cursor-pointer" />
