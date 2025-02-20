68bfe490ae2e60abeecafba502069702559f5edf
Valdir Silva <valdir.ti@gmail.com>
2025-02-20 19:26:53 -0300

    refact: change icon call variation icon component

--- src/app/ui/dashboard/exchange-rate/variation-icon.tsx
+++ src/app/ui/dashboard/exchange-rate/variation-icon.tsx
@@ -7,24 +7,20 @@ type VariationIconProps = {
 const VariationIcon = ({ value }: VariationIconProps) => {
   const numericValue = parseFloat(value.toFixed(2))
 
-  if (numericValue > 0) {
-    return (
+  const icons = {
+    positive: (
       <MdExpandLess
         data-testid="expand-less-icon"
         className="text-[28px] text-green-500 font-bold"
       />
-    )
-  }
-  if (numericValue < 0) {
-    return (
+    ),
+    negative: (
       <MdExpandMore
         data-testid="expand-more-icon"
         className="text-[28px] text-red-500 font-bold"
       />
-    )
-  }
-  if (numericValue === 0) {
-    return (
+    ),
+    neutral: (
       <MdHorizontalRule
         data-testid="expand-rule-icon"
         className="text-[28px] text-white-500 font-bold"
@@ -32,12 +28,11 @@ const VariationIcon = ({ value }: VariationIconProps) => {
     )
   }
 
-  return (
-    <MdHorizontalRule
-      data-testid="expand-rule-icon"
-      className="text-[28px] text-white-500 font-bold"
-    />
-  )
+  return numericValue > 0
+    ? icons.positive
+    : numericValue < 0
+      ? icons.negative
+      : icons.neutral
 }
 
 export default VariationIcon
