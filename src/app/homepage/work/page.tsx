bc6f66a1c3a43b5942f9257c6eb7f39a9738a00d
Valdir Silva <valdir.ti@gmail.com>
2025-03-25 17:33:55 -0300

    feat: screen height

--- src/app/homepage/work/page.tsx
+++ src/app/homepage/work/page.tsx
@@ -69,7 +69,7 @@ const Work = () => {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' }
       }}
-      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+      className="h-screen flex flex-col justify-between py-2 xl:px-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
