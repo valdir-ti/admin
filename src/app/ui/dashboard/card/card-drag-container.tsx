8ef2383c39b286c69268171a43baee0ccfa09e67
Valdir Silva <valdir.ti@gmail.com>
2025-04-06 11:18:10 -0300

    refact: use effect to look if localstorage exists

--- src/app/ui/dashboard/card/card-drag-container.tsx
+++ src/app/ui/dashboard/card/card-drag-container.tsx
@@ -1,19 +1,14 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Reorder } from 'framer-motion'
 
 import Card from './card'
 import { CardDragContainerProps, CardProps } from '@/types/card'
 
 export default function CardDragContainer({ data }: CardDragContainerProps) {
-  const storedIndicesString = localStorage.getItem('cardsOrder')
-  const initialItems = storedIndicesString
-    ? JSON.parse(storedIndicesString).map((index: string) =>
-        data.find((item) => item.index === index)
-      )
-    : data
-  const [items, setItems] = useState(initialItems)
+  const [items, setItems] = useState<CardProps[]>([])
+  const [loading, setLoading] = useState(true)
 
   const handleReorder = (data: CardProps[]) => {
     const indices = data?.map((item) => item.index)
@@ -21,24 +16,46 @@ export default function CardDragContainer({ data }: CardDragContainerProps) {
     setItems(data)
   }
 
+  useEffect(() => {
+    const storedIndicesString = localStorage.getItem('cardsOrder')
+
+    if (storedIndicesString) {
+      const storedIndices = JSON.parse(storedIndicesString)
+      const orderedItems = storedIndices
+        .map((index: string) => data.find((item) => item.index === index))
+        .filter(Boolean) as CardProps[]
+      setItems(orderedItems)
+    } else {
+      setItems(data)
+    }
+
+    setLoading(false)
+  }, [data])
+
   return (
     <div className="flex w-full gap-4">
-      <Reorder.Group
-        axis="x"
-        values={items}
-        onReorder={handleReorder}
-        className="grid grid-cols-3 w-full gap-4"
-      >
-        {items.map((item: CardProps) => (
-          <Reorder.Item
-            value={item}
-            key={item.index}
-            className="bg-[--bgSoft] hover:bg-[--bgHover] p-[4px] sm:p-4 rounded-md min-w-[32%] cursor-grab"
-          >
-            <Card data={item.data} index={item.index} />
-          </Reorder.Item>
-        ))}
-      </Reorder.Group>
+      {loading ? (
+        <div className="flex justify-center items-center w-full h-full">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <Reorder.Group
+          axis="x"
+          values={items}
+          onReorder={handleReorder}
+          className="grid grid-cols-3 w-full gap-4"
+        >
+          {items.map((item: CardProps) => (
+            <Reorder.Item
+              value={item}
+              key={item.index}
+              className="bg-[--bgSoft] hover:bg-[--bgHover] p-[4px] sm:p-4 rounded-md min-w-[32%] cursor-grab"
+            >
+              <Card data={item.data} index={item.index} />
+            </Reorder.Item>
+          ))}
+        </Reorder.Group>
+      )}
     </div>
   )
 }
