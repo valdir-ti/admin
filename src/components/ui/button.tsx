5238c3584c0a09ef808cb246874244032e4c83df
Valdir Silva <valdir.ti@gmail.com>
2026-07-28 22:35:26 -0300

    refact: update lint

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -28,7 +28,8 @@ const buttonVariants = cva(
 )
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
